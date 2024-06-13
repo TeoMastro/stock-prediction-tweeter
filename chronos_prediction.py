b6973feb929ce47e1c45c8c9082e969c5e667422
Eleftheriadis Emmanouil <manolis.eleftheriadis3@gmail.com>
2024-06-13 18:42:30 +0300

    chronos plot

--- chronos_prediction.py
+++ chronos_prediction.py
@@ -31,29 +31,30 @@ pipeline_small = ChronosPipeline.from_pretrained(
 data = pd.read_csv('data/AAPL.csv')
 filtered_data = data[['Date', 'Adj Close', 'ts_polarity', 'twitter_volume']]
 
-split_index = int(len(filtered_data) * 0.99)
+split_index = int(len(filtered_data) * 0.97)
 # Split the DataFrame
 historical_data = filtered_data.iloc[:split_index]
 testing_data = filtered_data.iloc[split_index:]
 
-list_context = [torch.tensor(historical_data['Adj Close']), torch.tensor(historical_data['ts_polarity']), torch.tensor(historical_data['twitter_volume'])]
+
+
+list_context = [torch.tensor(filtered_data['Adj Close']), torch.tensor(filtered_data['ts_polarity']), torch.tensor(filtered_data['twitter_volume'])]
 
 
 forecast = pipeline_small.predict(
     context=list_context,
-    prediction_length=24,
+    prediction_length=30,
     num_samples=20,
 )
 
 
 
 
-forecast_index = range(len(historical_data), len(historical_data) + 24)
+forecast_index = range(len(filtered_data), len(filtered_data) + 30)
 low, median, high = np.quantile(forecast[0].numpy(), [0.1, 0.5, 0.9], axis=0)
 
 plt.figure(figsize=(8, 4))
-plt.plot(historical_data['Adj Close'], color="royalblue", label="historical data")
-plt.plot(testing_data['Adj Close'], color="black", label="testing data")
+plt.plot(filtered_data['Adj Close'], color="royalblue", label="historical data")
 plt.plot(forecast_index, median, color="tomato", label="median forecast")
 plt.fill_between(forecast_index, low, high, color="tomato", alpha=0.3, label="80% prediction interval")
 plt.legend()
